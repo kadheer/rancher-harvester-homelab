58e7886613f0823e65c4991d5d6c653b549bff2b
kadirdevops <abdul.shaikh@student.jade-hs.de>
2026-04-20 20:24:11 +0200

    Create setup-harvester.sh for VM preparation
    Add setup script for Harvester installation instructions

---
+++ scripts/setup-harvester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Harvester Installations-Hinweise (keine vollautomatische ISO-Installation)
+# Dieses Skript bereitet nur die VM für die Harvester-ISO vor.
+
+echo "Bitte boote die Harvester-ISO manuell oder via virt-install."
+echo "Nach der Installation wird Harvester unter https://<IP> erreichbar sein."
+echo "Benutzer: admin / Passwort: <setze während der Installation>"
